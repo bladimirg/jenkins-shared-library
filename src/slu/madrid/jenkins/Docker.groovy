2182009d9c32aba2eeeaf3b7118ceb03051c5e46
Bladimir Gonzales <bladimirg@gmail.com>
2023-03-24 02:00:32 +0100

    Update Docker.groovy

--- src/slu/madrid/jenkins/Docker.groovy
+++ src/slu/madrid/jenkins/Docker.groovy
@@ -13,7 +13,7 @@ class Docker implements Serializable {
 
   def dockerLogin(){
     script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-      sh "echo $script.PASS | docker login  -u $script.USER --password-stdin localhost:5000"
+      sh "echo $script.PASS | docker login  -u $script.USER -p --password-stdin localhost:5000"
 
     }
   }
